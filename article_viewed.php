1a8925a9bbc2b579ad9f4f102a0ffc62fe2e90a3
pomalone91 <pomalone91@gmail.com>
2022-01-01 09:01:42 -0700

    Adding new script to track views

---
+++ article_viewed.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+EVERY TIME AN ARTICLE IS CLICKED, THIS SCRIPT WILL RUN TO ADD A NEW RECORD TO VIEWS TABLE
+
+Views table columns...
+- viewId            INT PK
+- articleId         INT FK
+- viewedAt          DATETIME
+- view_year         INT
+- view_month        INT
+- view_day          INT
+- view_quarter      VARCHAR(2)
+- view_yearquarter  VARCHAR(6)
+ 
+*/
+?>
