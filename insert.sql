f54766a810e754f01d0647989fcb4d2b6ebaf6aa
pomalone91 <pomalone91@gmail.com>
2022-01-01 09:39:23 -0700

    sql script for creating views table

--- insert.sql
+++ insert.sql
@@ -20,4 +20,18 @@ WHERE id IN (9)
 UPDATE user SET authentication_string = PASSWORD('plog90'), password_last_changed = NULL
 WHERE user.Host = 'localhost' AND user.User = 'root';
 
-INSERT INTO blog.users (username, password, addedAt) VALUES ('admin', '5810027b75b1281dbdea90b4e4ff60fa', CURRENT_DATE())
+INSERT INTO blog.users (username, password, addedAt) VALUES ('admin', '5810027b75b1281dbdea90b4e4ff60fa', CURRENT_DATE());
+
+-- Create view table
+CREATE TABLE blog.views (
+    viewId              INT AUTO_INCREMENT
+    ,articleId          INT
+    ,viewedAt           DATETIME
+    ,view_year          INT
+    ,view_month         INT
+    ,view_day           INT
+    ,view_quarter       VARCHAR(2)
+    ,view_time          TIME
+    ,PRIMARY KEY (viewId)
+    ,FOREIGN KEY (articleId) REFERENCES articles(id)
+);
